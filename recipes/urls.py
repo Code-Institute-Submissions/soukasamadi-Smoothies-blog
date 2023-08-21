46f75ea1d077cd0b7885d4481db1db98aa36bfe6
soukasamadi <>
2023-08-21 09:22:12 +0000

    Add views, urls, forms and recipe_details files

---
+++ recipes/urls.py
@@ -0,0 +1,18 @@
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path
+from .forms import CommentForm
+
+from .views import *
+
+"""url paths"""
+urlpatterns = [
+    path('', views.index, name="index"),
+    path('<slug:slug>/', views.RecipeDetail.as_view(), name='recipe_detail'),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,83 @@
-from django.shortcuts import render
+from django.views import generic, View
+from .models import *
+from .forms import CommentForm, UserUpdateForm, ProfileUpdateForm
+from django.shortcuts import (
+    render, get_object_or_404, reverse, redirect, resolve_url)
 
-# Create your views here.
+
+def index(request):
+    """
+    Renders the index page
+    """
+    queryset = Recipe.objects.filter(
+        featured=True, status=1).order_by('-timestamp')
+    context = {
+        'recipe_list': queryset,
+    }
+    return render(request, 'recipes/index.html', context)
+
+
+class RecipeDetail(View):
+    """
+    Renders the post detail Page
+    """
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Recipe.objects.filter(status=1)
+        recipe = get_object_or_404(queryset, slug=slug)
+        comments = recipe.comments.filter(approved=True).order_by("-timestamp")
+        num_comments = Comment.objects.filter(recipe=recipe).count()
+        liked = False
+        if recipe.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "recipes/recipe_detail.html",
+            {
+                "recipe": recipe,
+                "comments": comments,
+                "commented": False,
+                "liked": liked,
+                "comment_form": CommentForm(),
+                'num_comments': num_comments,
+            },
+        )
+
+    def recipe(self, request, slug, *args, **kwargs):
+        """
+        Comment on the posts
+        """
+        queryset = Recipe.objects.filter(status=1)
+        recipe = get_object_or_404(queryset, slug=slug)
+        comments = recipe.comments.filter(approved=True).order_by("-timestamp")
+        liked = False
+
+        if recipe.likes.filter(id=self.request.user.id).exists():
+            liked = True
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+            comment_form.instance.name = request.user.username
+            comment = comment_form.save(commit=False)
+            comment.recipe = recipe
+            comment.save()
+            messages.success(request, """
+            Your comment was sent successfully and is awaiting approval!""")
+        else:
+            comment_form = CommentForm()
+        return render(
+            request,
+            "recipes/recipe_detail.html",
+            {
+                "recipe": recipe,
+                "comments": comments,
+                "commented": True,
+                "comment_form": comment_form,
+                "liked": liked,
+            },
+        )
+
+        post
+
+
