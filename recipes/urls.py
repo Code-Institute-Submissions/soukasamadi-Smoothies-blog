08b84edb66ad5f88cf1fadaac50b4f7cb0ea47a2
soukasamadi <>
2023-08-23 19:39:20 +0000

    Add view, forms, url path and admin for profile page

--- recipes/urls.py
+++ recipes/urls.py
@@ -20,6 +20,7 @@ urlpatterns = [
     path('<slug:slug>/', views.RecipeDetail.as_view(), name='recipe_detail'),
     path('like/<slug:slug>', views.RecipeLike.as_view(), name='recipe_like'),
     path('accounts/', include('allauth.urls')),
+    path('profile', views.ProfileView, name='profile'),
 ]
 
 if settings.DEBUG:
--- recipes/views.py
+++ recipes/views.py
@@ -144,6 +144,31 @@ def categories_view(request, cats):
         'cats': cats.title(), 'categories_posts': categories_posts})
 
 
+def ProfileView(request):
+    """View to return the profile page"""
+    if request.method == 'POST':
+      user_form = UserUpdateForm(request.POST, instance=request.user)
+      profile_form = ProfileUpdateForm(request.POST,
+                                       request.FILES,
+                                       instance=request.user.profile)
+      if user_form.is_valid() and profile_form.is_valid():
+          user_form.save()
+          profile_form.save()
+          messages.success(request, f"Your account has been updated!")
+          return redirect('profile')
+    else:
+      user_form = UserUpdateForm(instance=request.user)
+      profile_form = ProfileUpdateForm(instance=request.user.profile)
+
+    categories = Category.objects.all()
+    context = {
+        'categories_list': categories,
+        'user_form': user_form,
+        'profile_form': profile_form,
+    }
+    return render(request, 'profile.html', context)
+
+
 def search(request):
     """search results"""
     queryset = Post.objects.all()
