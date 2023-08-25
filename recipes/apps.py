8f15d2a5425b0fe09883cb9704d399e0c8ed4468
soukasamadi <>
2023-08-25 01:13:45 +0000

    Create redy function

--- recipes/apps.py
+++ recipes/apps.py
@@ -4,3 +4,11 @@ from django.apps import AppConfig
 class RecipesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'recipes'
+
+    def ready(self):
+        """
+        Gets the user's details
+        """
+        import recipes.signals
+
+
