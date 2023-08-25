2e8e3e086510d5252f8a39178cc8a69f13cec7fa
soukasamadi <>
2023-08-25 22:06:29 +0000

    Create fiels for challenge in admin panel

--- challenge/admin.py
+++ challenge/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Challenge
 
-# Register your models here.
+
+@admin.register(Challenge)
+class BookAdmin(admin.ModelAdmin):
+    """
+    Add fields for Challenge in admin panel
+    """
+    list_display = ('name', 'goal', 'timestamp', 'approved')
+    list_filter = ('approved', 'timestamp')
+    actions = ['approve_challenge']
+
+    def approve_challenge(self, request, queryset):
+        queryset.update(approved=True)
